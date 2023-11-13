6effd4bab663eaf6bb3e810f1c561b428fdd9545
kavithakarnam <150236859+kavithakarnam@users.noreply.github.com>
2023-11-13 15:46:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Window alert message
+window.alert(" Hello,This is the Task-5 kik start program");
+
+//printing string in js
+console.log("Hello, This is a string");
+
+//variables 
+
+var1="25";
+var2="30";
+var3="40";
+
+console.log(var1);
+console.log(var2);
+
+
+//camelCase
+
+weightOfAPerson=60;
+console.log(weightOfAPerson);
+
+//variables with proper declaration
+let name="kavitha";
+let age=25;
+let height=4.8;
+console.log(name);
+
+var name1="kavitha"
+var age1=25;
+var height1=4.8;
+console.log(height1);
+
+
+const PI=4.5;
+console.log(PI);
